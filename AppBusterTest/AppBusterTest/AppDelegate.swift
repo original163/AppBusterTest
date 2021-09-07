9aad7665dac54fdfac16c6eab5caac5c779e12e7
original163 <original163@mail.ru>
2021-09-07 23:05:21 +0400

    created sample project

---
+++ AppBusterTest/AppBusterTest/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  AppBusterTest
+//
+//  Created by Денис Денисов on 07.09.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    
+    
+        return true
+    }
+
+}
+
+
---
+++ AppBusterTest/AppBusterTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppBusterTest
+//
+//  Created by Денис Денисов on 07.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
